b44b84fe1337d96c61ba32c953a559af8f1c0bd2
Parnika Roy <parnika.roy30@gmail.com>
2025-02-21 22:11:36 +0530

    initial commit

---
+++ src/main/java/com/designPatterns/Application.java
@@ -0,0 +1,12 @@
+package com.designPatterns;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
